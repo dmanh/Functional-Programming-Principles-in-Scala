256f6436dc4e8fdc92cf53e5eead491bcf5e62a0
wjf3121 <wjf3121@hotmail.com>
2014-04-29 22:57:00 +0800

    week 1

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,27 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
-
+  def pascal(c: Int, r: Int): Int = 
+    if(r==0||c==0||c==r) 1 else pascal(c-1,r-1)+pascal(c,r-1)
+  
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
-
+  def balance(chars: List[Char]): Boolean = {
+    def balanceIter(leftNum: Int, chars: List[Char]): Boolean = 
+    	if(chars.isEmpty) leftNum==0
+    	else if(chars.head == '(') balanceIter(leftNum+1,chars.tail)
+    	else if(chars.head == ')') leftNum>0 && balanceIter(leftNum-1,chars.tail) 
+    	else balanceIter(leftNum,chars.tail) 
+    balanceIter(0,chars)
+  }
+  
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = 
+    if(money == 0) 1
+    else if(money<0||coins.isEmpty) 0
+    else countChange(money - coins.head,coins) + 
+    	countChange(money,coins.tail)
 }
